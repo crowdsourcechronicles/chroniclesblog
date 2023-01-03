a3c96d0f3ec85a9512f3b9d42c6bf5ab8a9ef716
Jeremy <jdskinne@gmail.com>
2023-01-03 13:58:34 -0600

    added main handlebars and models

---
+++ models/index.js
@@ -0,0 +1,29 @@
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+Post.belongsTo(User, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'postId',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'userId',
+  onDelete: 'CASCADE'
+});
+
+User.hasMany(Post, {
+  foreignKey: 'postId',
+  onDelete: 'CASCADE'
+});
+
+module.exports = {
+  User,
+  Comment,
+  Post
+};
